8d455d806e148cb51cddf8428e656842e65d36ff
James Davidheiser <jdavidheiser@gmail.com>
2013-07-08 17:33:13 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+// If we set default text, we know that we didn't read the file correctly
+var filebuffer = new Buffer("Default Text");
+var fs = require('fs')
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  fd = fs.open('index.html','r')
+  fs.readSync(fd, filebuffer)
+  response.send(filebuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
